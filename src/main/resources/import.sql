a5a8786338b9c4fde78d0f6f7350bdcc75475876
zzimme <zzimme@gmail.com>
2017-06-11 00:27:42 +0900

    오타 수정.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into `type` (name) values ("원품(오픈형)");
-insert into `type` (name) values ("원품(분리형)");
-insert into `type` (name) values ("원품(복층형)");
+insert into `type` (name) values ("원룸(오픈형)");
+insert into `type` (name) values ("원룸(분리형)");
+insert into `type` (name) values ("원룸(복층형)");
 insert into `type` (name) values ("투룸");
 insert into `type` (name) values ("쓰리룸");
 insert into `type` (name) values ("오피스텔");
