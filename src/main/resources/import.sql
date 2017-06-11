2f55209f9f0473c3430564f2abad53d6c161396c
zzimme <zzimme@gmail.com>
2017-06-11 14:33:12 +0900

    refactoring

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into `type` (name) values ("원룸(오픈형)");
-insert into `type` (name) values ("원룸(분리형)");
-insert into `type` (name) values ("원룸(복층형)");
-insert into `type` (name) values ("투룸");
-insert into `type` (name) values ("쓰리룸");
-insert into `type` (name) values ("오피스텔");
